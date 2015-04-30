a59a73e98c602177469d68c120733d8ba2e4a5ed
Rich Towers <rich.towers@crunch.co.uk>
2015-04-30 10:03:39 +0100

    NSS-1106 - Rename to 'Cross Site Link Search'
    Method cannot have the same name as the class

--- cross-site-link-search.php
+++ cross-site-link-search.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Plugin Name: Extra Link Search
- * Description: Allows one to add extra APIs to the link search box
- * Version: 0.0.1
+ * Plugin Name: Cross Site Link Search
+ * Description: Add other wordpress sites to the link search results.
+ * Version: 0.0.3
  * Author: Crunch
  * Author URI: http://www.crunch.co.uk/
  *
- * @package ExtraLinkSearch
+ * @package CrossSiteLinkSearch
  * @author Crunch Accounting <info@crunch.co.uk>
  * @copyright Copyright (c) 2015, E-Crunch Ltd.
  */
@@ -16,10 +16,10 @@
  * Load the plugin.
  */
 if ( is_admin() ) {
-	add_action( 'plugins_loaded', array( 'Extra_Link_Search', 'load' ) );
+	add_action( 'plugins_loaded', array( 'Cross_Site_Link_Search', 'load' ) );
 }
 
-class Extra_Link_Search {
+class Cross_Site_Link_Search {
 
 	private static $key;
 	private static $routesToSearch;
@@ -32,14 +32,14 @@ class Extra_Link_Search {
 		// Add the options page:
 		add_action( 'admin_menu', array( __CLASS__, 'add_options_page' ) );
 
-		self::$key = get_option( 'els_key' );
-		self::$routesToSearch = explode(',', get_option( 'els_routes' ));
-		$exposeSearch = get_option( 'els_expose_search' );
+		self::$key = get_option( 'csls_key' );
+		self::$routesToSearch = explode(',', get_option( 'csls_routes' ));
+		$exposeSearch = get_option( 'csls_expose_search' );
 
 		if ($exposeSearch) {
 			// Expose the link search to other WP installs with the correct key:
-			add_action( 'wp_ajax_extra_get_link_search_results', array( __CLASS__, 'extra_get_link_search_results' ) );
-			add_action( 'wp_ajax_nopriv_extra_get_link_search_results', array( __CLASS__, 'extra_get_link_search_results' ) );
+			add_action( 'wp_ajax_cross_site_link_search', array( __CLASS__, 'cross_site_link_search_fn' ) );
+			add_action( 'wp_ajax_nopriv_cross_site_link_search', array( __CLASS__, 'cross_site_link_search_fn' ) );
 		}
 
 		// Replace the default wp-link-ajax action.
@@ -52,58 +52,58 @@ class Extra_Link_Search {
 
 	public static function add_settings () {
 		add_settings_section(
-			'els_group',
-			'Extra Link Search',
-			array(__CLASS__, 'els_group_callback'),
-			'extra-link-search' );
+			'csls_group',
+			'Cross Site Link Search',
+			array(__CLASS__, 'csls_group_callback'),
+			'cross-site-link-search' );
 
 		add_settings_field(
-			'els_expose_search',
+			'csls_expose_search',
 			'Expose link search',
-			array(__CLASS__, 'els_expose_search_callback'),
-			'extra-link-search',
-			'els_group'
+			array(__CLASS__, 'csls_expose_search_callback'),
+			'cross-site-link-search',
+			'csls_group'
 		);
 
 		add_settings_field(
-			'els_key',
+			'csls_key',
 			'Secret key',
-			array(__CLASS__, 'els_key_callback'),
-			'extra-link-search',
-			'els_group'
+			array(__CLASS__, 'csls_key_callback'),
+			'cross-site-link-search',
+			'csls_group'
 		);
 
 		add_settings_field(
-			'els_routes',
+			'csls_routes',
 			'Routes to search',
-			array(__CLASS__, 'els_routes_callback'),
-			'extra-link-search',
-			'els_group'
+			array(__CLASS__, 'csls_routes_callback'),
+			'cross-site-link-search',
+			'csls_group'
 		);
 
-		register_setting( 'els_group', 'els_expose_search' );
-		register_setting( 'els_group', 'els_key' );
-		register_setting( 'els_group', 'els_routes' );
+		register_setting( 'csls_group', 'csls_expose_search' );
+		register_setting( 'csls_group', 'csls_key' );
+		register_setting( 'csls_group', 'csls_routes' );
 	}
 
-	public static function els_group_callback () {
+	public static function csls_group_callback () {
 
 	}
 
-	public static function els_expose_search_callback () {
-		echo '<input name="els_expose_search" id="els_expose_search" type="checkbox" value="1" class="code" ' . checked( 1, get_option( 'els_expose_search' ), false ) . ' /> Whether this installation should allow other wordpress installs with the same key to see its link search';
+	public static function csls_expose_search_callback () {
+		echo '<input name="csls_expose_search" id="csls_expose_search" type="checkbox" value="1" class="code" ' . checked( 1, get_option( 'csls_expose_search' ), false ) . ' /> Whether this installation should allow other wordpress installs with the same key to see its link search';
 	}
 
-	public static function els_key_callback () {
-		echo '<input name="els_key" id="els_key" type="text" class="code" value="' . get_option( 'els_key' )  . '" /> A key which matches that in the other installations. Generate with <code>openssl rand -base64 32</code>';
+	public static function csls_key_callback () {
+		echo '<input name="csls_key" id="csls_key" type="text" class="code" value="' . get_option( 'csls_key' )  . '" /> A key which matches that in the other installations. Generate with <code>openssl rand -base64 32</code>';
 	}
 
-	public static function els_routes_callback () {
-		echo '<input name="els_routes" id="els_routes" type="text" class="code" value="' . get_option( 'els_routes' )  . '" /> A comma separated list of routes to wordpress installs to search for links in. Relative to the site root (e.g. /blog)';
+	public static function csls_routes_callback () {
+		echo '<input name="csls_routes" id="csls_routes" type="text" class="code" value="' . get_option( 'csls_routes' )  . '" /> A comma separated list of routes to wordpress installs to search for links in. Relative to the site root (e.g. /blog)';
 	}
 
 	public static function add_options_page() {
-		add_options_page( 'Extra Link Search Options', 'Extra Link Search', 'manage_options', 'extra-link-search', array( __CLASS__, 'options' ) );
+		add_options_page( 'Cross Site Link Search Options', 'Cross Site Link Search', 'manage_options', 'cross-site-link-search', array( __CLASS__, 'options' ) );
 	}
 
 	public static function options() {
@@ -111,8 +111,8 @@ class Extra_Link_Search {
 			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 		}
 		echo '<form method="POST" action="options.php">';
-		settings_fields( 'els_group' );
-		do_settings_sections( 'extra-link-search' );
+		settings_fields( 'csls_group' );
+		do_settings_sections( 'cross-site-link-search' );
 		submit_button();
 		echo '</form>';
 	}
@@ -141,7 +141,7 @@ class Extra_Link_Search {
 		wp_die();
 	}
 
-	public static function extra_get_link_search_results() {
+	public static function cross_site_link_search_fn() {
 		if ($_POST['key'] !== self::$key) {
 			wp_die( -1 );
 		}
@@ -182,7 +182,7 @@ class Extra_Link_Search {
 		// Copy $_POST so we can mutate it without being bad citizens:
 		$request = $_POST;
 
-		$request['action'] = 'extra_get_link_search_results';
+		$request['action'] = 'cross_site_link_search';
 		$request['key'] = self::$key;
 
 		// Forward the POST on to the other API:
